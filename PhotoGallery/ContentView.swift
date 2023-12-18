f5a5c6da4ebe5d9d4c467bf8fd708cb8b7a86688
bubblecocoa <bubblecocoa@gmail.com>
2023-12-18 12:07:14 +0900

    refactor: numberOfColuns 프로퍼티 추가하여  Menu 내의 반복 코드 제거

--- PhotoGallery/ContentView.swift
+++ PhotoGallery/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     private static let initailColumns = 4
     
+    @State private var numberOfColuns = initailColumns
     @State private var gridColumns = Array(repeating: GridItem(.flexible(), spacing: 1), count: initailColumns)
     
     var body: some View {
@@ -36,22 +37,14 @@ struct ContentView: View {
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 Menu {
-                    Button {
-                        changeColumns(3)
-                    } label: {
-                        Text("3개로 보기")
-                    }
-                    
-                    Button {
-                        changeColumns(4)
-                    } label: {
-                        Text("4개로 보기")
-                    }
-                    
-                    Button {
-                        changeColumns(5)
-                    } label: {
-                        Text("5개로 보기")
+                    ForEach((3...5), id: \.self) { number in
+                        if numberOfColuns != number {
+                            Button {
+                                changeColumns(number)
+                            } label: {
+                                Text("\(number)개로 보기")
+                            }
+                        }
                     }
                 } label: {
                     Image(systemName: "circle.grid.3x3.circle")
@@ -63,6 +56,7 @@ struct ContentView: View {
     
     private func changeColumns(_ count: Int) {
         withAnimation {
+            numberOfColuns = count
             gridColumns = Array(repeating: GridItem(.flexible(), spacing: 1), count: count)
         }
     }
