e0744c0d7a85c1f09a8a767743a0e94529a08af5
bubblecocoa <bubblecocoa@gmail.com>
2023-12-18 12:03:12 +0900

    refactor: toolbar 적용을 위해 rootView를 NavigationStack으로 wrapping

--- PhotoGallery/PhotoGalleryApp.swift
+++ PhotoGallery/PhotoGalleryApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct PhotoGalleryApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+            }
         }
     }
 }
